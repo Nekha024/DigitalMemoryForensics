a17cf56cf1278f64abbab8a088bf38385d474c9d
Nekha024 <nekha.a2003@gmail.com>
2026-04-29 00:18:35 +0530

    designs

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,8 +3,6 @@ from django.contrib.auth.views import LogoutView
 from .views import  verify_otp,auth_page
 
 urlpatterns = [
-    # path("login/", custom_login, name="custom_login"),
-    # path("register/", register, name="register"),
     path("auth/", auth_page, name="auth"),
     path("verify-otp/<int:user_id>/", verify_otp, name="verify_otp"),
     path("logout/", LogoutView.as_view(next_page="auth"), name="logout"),
